ac782eebb5d13e62e1426529792e7b2e4d406281
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-31 11:35:48 +0100

    Style NavBar link with classnames package

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,13 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+
+  const currentPath = usePathname()
 
   const links = [
     { label: 'Dashboard', href: '/'},
@@ -14,7 +20,11 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {links.map(link => (
           <li key={link.href}>
-            <Link className='text-zinc hover:text-zinc-800 transition-colors'
+            <Link className={classnames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true
+            })}
                   href={link.href}>
                     {link.label}
             </Link>
@@ -26,3 +36,5 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+// `${link.href===currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors`
